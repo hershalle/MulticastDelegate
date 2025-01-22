36a40273ad35ab1a2007ae3f64677a87895dce63
Shai Balassiano <hershalle@gmail.com>
2025-01-22 13:59:15 +0200

    Try to fix SPM

--- Package.swift
+++ Package.swift
@@ -1,22 +1,27 @@
 // swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "MulticastDelegate",
     platforms: [
-        .iOS(.v17),
-        .macOS(.v13),
-    ],
+        .iOS(.v13),
+        ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MulticastDelegate",
             targets: ["MulticastDelegate"]),
     ],
-    dependencies: [],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MulticastDelegate",
-            dependencies: []),
+            name: "MulticastDelegate"),
+        .testTarget(
+            name: "MulticastDelegateTests",
+            dependencies: ["MulticastDelegate"]
+        ),
     ]
 )
